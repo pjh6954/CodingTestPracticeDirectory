8076727b83d984325d54de332067887cc692ca80
junhoPark <pjh7678@naver.com>
2021-05-07 12:25:23 +0900

    VideoEditorApp project add

---
+++ App/VideoEditorApp/VideoEditorApp/VideoEditorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoEditorApp
+//
+//  Created by Dannian Park on 2021/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
