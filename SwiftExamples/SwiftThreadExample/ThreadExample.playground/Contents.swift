1085664a3d6d13d4fcc98b93d72be60d394b838f
junhoPark <pjh7678@naver.com>
2021-05-07 11:25:43 +0900

    DispatchQueue Test Code
    Dispatch queue test code add.

--- SwiftExamples/SwiftThreadExample/ThreadExample.playground/Contents.swift
+++ SwiftExamples/SwiftThreadExample/ThreadExample.playground/Contents.swift
@@ -1,3 +1,43 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+let displayImojiArr = [
+    "263A", // ☺
+    "1F600", // 😀
+    "1F638", // 😸
+    "1F631" // 😱
+]
+/*
+// Test 01
+DispatchQueue.main.async {
+    testCode(0)
+}
+
+DispatchQueue.main.async {
+    testCode(1)
+}
+*/
+/*
+DispatchQueue.global().async {
+    testCode(0)
+}
+DispatchQueue.global().async {
+    testCode(1)
+}
+*/
+DispatchQueue.global().sync {
+    testCode(2)
+}
+DispatchQueue.global().sync {
+    testCode(3)
+}
+DispatchQueue.global().async {
+    testCode(0)
+}
+DispatchQueue.global().async {
+    testCode(1)
+}
+func testCode(_ type : Int) {
+    for i in 0 ..< 5 {
+        print(String(UnicodeScalar(Int(displayImojiArr[type], radix: 16)!)!) + "\(i + 1)")
+    }
+}
