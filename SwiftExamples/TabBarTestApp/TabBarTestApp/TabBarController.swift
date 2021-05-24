25fee9015211087e26c2b3d58aa8694bcefb7540
junhoPark <37360920+pjh6954@users.noreply.github.com>
2021-05-24 17:50:55 +0900

    TabBar 관련 테스트 어플리케이션 구현

---
+++ SwiftExamples/TabBarTestApp/TabBarTestApp/TabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarController.swift
+//  TabBarTestApp
+//
+//  Created by Dannian Park on 2021/05/24.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SwiftExamples/TabBarTestApp/TabBarTestApp/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  TabBarTestApp
+//
+//  Created by Dannian Park on 2021/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let viewModel = ViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        viewModelBinding()
+    }
+    
+    private func viewModelBinding() {
+        self.viewModel.moveReturn = { [weak self] in
+            guard let strongSelf = self else {
+                return
+            }
+            print("StrongSelf! : \(strongSelf)")
+            guard let tabBarController = UIStoryboard(name: "TabBarViewController", bundle: nil).instantiateViewController(withIdentifier: "TabBarController") as? TabBarController else {
+                return
+            }
+            strongSelf.present(tabBarController, animated: true) {
+                
+            }
+        }
+    }
+    
+    @IBAction func actionPresent(_ sender: UIButton) {
+        viewModel.moveFunction()
+    }
+}
+
+protocol viewModelProtocol {
+    func moveFunction()
+    var moveReturn: (() -> Void)? { get }
+}
+
+class ViewModel : viewModelProtocol {
+    
+    func moveFunction() {
+        self.moveReturn?()
+    }
+    
+    //output
+    var moveReturn: (() -> Void)?
+}
