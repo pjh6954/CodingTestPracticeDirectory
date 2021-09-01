a605ae4f0bdc31cf23ceba466d26a602e840694c
junhoPark <37360920+pjh6954@users.noreply.github.com>
2021-09-02 00:46:00 +0900

    QOS와 DispatchQueue.main 상관관계, QOS별 처리 우선순위

--- SwiftExamples/Examples/TheoryStudyProject/TheoryStudyProject/ViewController.swift
+++ SwiftExamples/Examples/TheoryStudyProject/TheoryStudyProject/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let stv : UIStackView = {
+        let stv = UIStackView()
+        stv.translatesAutoresizingMaskIntoConstraints = false
+        stv.spacing = 10
+        stv.axis = .vertical
+        return stv
+    }()
     private let testView : UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .green
         return view
     }()
+    
+    private let testSecondView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .blue
+        return view
+    }()
+    
     // MARK: - VC Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +45,8 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         print("\(#fileID)::\(#function)")
-        self.drawLayerInBackgroundTest()
+        //self.drawLayerInBackgroundTest()
+        self.multipleThreadWithSync()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -56,6 +72,16 @@ class ViewController: UIViewController {
     // My Functions
     private func commonInit() {
         print("\(#fileID)::\(#function)")
+        // self.drawingOneView()
+        self.drawingTwoView()
+    }
+    
+    
+}
+// MARK: - Drawing View
+extension ViewController {
+    private func drawingOneView() {
+        // center only one view display
         self.view.addSubview(self.testView)
         NSLayoutConstraint.activate([
             .init(item: testView, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 0),
@@ -64,7 +90,22 @@ class ViewController: UIViewController {
             .init(item: testView, attribute: .width, relatedBy: .equal, toItem: testView, attribute: .height, multiplier: 1.5, constant: 0)
         ])
     }
-    
+    private func drawingTwoView() {
+        self.view.addSubview(self.stv)
+        self.stv.distribution = .fillEqually
+        NSLayoutConstraint.activate([
+            .init(item: self.stv, attribute: .width, relatedBy: .equal, toItem: self.view, attribute: .width, multiplier: 0.7, constant: 0),
+            .init(item: self.stv, attribute: .height, relatedBy: .equal, toItem: self.view, attribute: .height, multiplier: 0.5, constant: 0),
+            .init(item: self.stv, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 0),
+            .init(item: self.stv, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: 0)
+        ])
+        self.stv.addArrangedSubview(self.testView)
+        self.stv.addArrangedSubview(self.testSecondView)
+    }
+}
+
+extension ViewController {
+    // MARK: - Thread와 관련된 CALayer 테스트
     private func drawLayerInBackgroundTest() {
         print("\(#fileID)::\(#function)")
         func drawing() {
@@ -98,6 +139,102 @@ class ViewController: UIViewController {
         //tesqueue.async { drawing() }
         tesqueue.sync { drawing() }
         // sync가 되면 무소건 thread1에서 처리가 되는 것을 확인함. 이런 이유로 인해 disqueue.global() 등 여러 방법을 써도 sync를 호출하면 dispatchqueue.main.async와 동일한 결과를 도출 하는 것으로 확인.
+    }
+    
+    // MARK: - 각 쓰레드.sync와 우선순위 간의 차이여부
+    private func multipleThreadWithSync() {
+        func drawingColorSwitching(_ view: UIView, color: UIColor) {
+            view.layer.borderWidth = 4
+            view.layer.borderColor = view.backgroundColor?.cgColor
+            view.backgroundColor = color
+        }
+        
+        /// 이 경우는 결국 sync이고, thread1에서 모두 처리하기 때문에 순서대로 동작하게 된다.
+        /*
+        DispatchQueue.global(qos: .background).sync {
+            drawingColorSwitching(self.testView, color: .yellow)
+            drawingColorSwitching(self.testView, color: .purple)
+            drawingColorSwitching(self.testView, color: .black)
+            drawingColorSwitching(self.testView, color: .magenta)
+            drawingColorSwitching(self.testView, color: .blue)
+        }
+        DispatchQueue.global(qos: .userInteractive).sync {
+            drawingColorSwitching(self.testSecondView, color: .red)
+            drawingColorSwitching(self.testSecondView, color: .orange)
+            drawingColorSwitching(self.testSecondView, color: .darkGray)
+            drawingColorSwitching(self.testSecondView, color: .gray)
+            drawingColorSwitching(self.testSecondView, color: .green)
+        }
+        */
+        // 초반에는 먼저 시작한 쪽이 좀 더 많이 수행되는 듯 하나(특히 main에서는) 나중되면 qos에 따라 처리 순서가 달라지기 시작함.(대체로)
+        DispatchQueue.global(qos: .background).async {
+            
+            DispatchQueue.main.async {
+                print("Background - 1")
+                drawingColorSwitching(self.testView, color: .yellow)
+            }
+            //sleep(1)
+            DispatchQueue.main.async {
+                print("Background - 2")
+                drawingColorSwitching(self.testView, color: .purple)
+            }
+            //sleep(1)
+            DispatchQueue.main.async {
+                print("Background - 3")
+                drawingColorSwitching(self.testView, color: .black)
+            }
+            //sleep(1)
+            DispatchQueue.main.async {
+                print("Background - 4")
+                drawingColorSwitching(self.testView, color: .magenta)
+            }
+            //sleep(1)
+            DispatchQueue.main.async {
+                print("Background - 5")
+                drawingColorSwitching(self.testView, color: .blue)
+            }
+            
+            for i in 1 ... 10 {
+                print("BACKGROUND FOR :: \(i)")
+                defer {
+                    print("DEFER ::: Background :: \(i)")
+                }
+            }
+        }
+        
+        DispatchQueue.global(qos: .userInteractive).async {
+            DispatchQueue.main.async {
+                print("UserInteractive - 1")
+                drawingColorSwitching(self.testSecondView, color: .red)
+            }
+            //sleep(1)
+            DispatchQueue.main.async {
+                print("UserInteractive - 2")
+                drawingColorSwitching(self.testSecondView, color: .orange)
+            }
+            //sleep(1)
+            DispatchQueue.main.async {
+                print("UserInteractive - 3")
+                drawingColorSwitching(self.testSecondView, color: .darkGray)
+            }
+            //sleep(1)
+            DispatchQueue.main.async {
+                print("UserInteractive - 4")
+                drawingColorSwitching(self.testSecondView, color: .gray)
+            }
+            //sleep(1)
+            DispatchQueue.main.async {
+                print("UserInteractive - 5")
+                drawingColorSwitching(self.testSecondView, color: .green)
+            }
+            
+            for i in 1 ... 10 {
+                print("USERINTERACTIVE FOR :: \(i)")
+                defer {
+                    print("DEFER ::: UserInteractive :: \(i)")
+                }
+            }
+        }
     }
 }
 
